e0ad2af0ba3794e6b24a0d2338f802e99e5f747a
despencer <despencer@mail.ru>
2022-11-23 12:29:36 +0300

    gazebo test build

---
+++ gazebo/src/gazebo/main.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include "Console.hh"
+// #include "Master.hh"
+
+bool setupServer()
+{
+  gzlog << "gazebo::setupServer started\n";
+/*  std::string host = "";
+  unsigned int port = 0;
+
+  gazebo::transport::get_master_uri(host, port);
+
+  g_master = new gazebo::Master();
+  g_master->Init(port);
+  gzlog << "gazebo::setupServer ready to start master thread\n";
+  g_master->RunThread();
+
+  gzlog << "gazebo::setupServer finished\n";*/
+  std::cout << "Can kill me now" << std::endl;
+  while(1)
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+  return true;
+}
+
+int main()
+{
+ std::cout << "main started" << std::endl;
+ setupServer();
+ return 0;
+}
