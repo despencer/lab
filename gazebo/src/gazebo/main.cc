6471a0dbe3fa37815d6689e624786e1c90e8ee13
despencer <despencer@mail.ru>
2022-11-29 23:05:13 +0300

    Build with Master.cc included.

--- gazebo/src/gazebo/main.cc
+++ gazebo/src/gazebo/main.cc
@@ -3,25 +3,26 @@
 #include <chrono>
 #include "gazebo/util/LogRecord.hh"
 #include "gazebo/common/Console.hh"
-// #include "Master.hh"
+#include "gazebo/transport/TransportIface.hh"
+#include "Master.hh"
 
 bool setupServer()
 {
-    gzLogInit("test-", "gzserver.log");
-    gazebo::util::LogRecord::Instance()->Init("gzserver");
+  gzLogInit("test-", "gzserver.log");
+  gazebo::util::LogRecord::Instance()->Init("gzserver");
 
   gzlog << "gazebo::setupServer started\n";
-/*  std::string host = "";
+  std::string host = "";
   unsigned int port = 0;
 
   gazebo::transport::get_master_uri(host, port);
 
-  g_master = new gazebo::Master();
+  gazebo::Master* g_master = new gazebo::Master();
   g_master->Init(port);
   gzlog << "gazebo::setupServer ready to start master thread\n";
   g_master->RunThread();
 
-  gzlog << "gazebo::setupServer finished\n";*/
+  gzlog << "gazebo::setupServer finished\n";
   std::cout << "Can kill me now" << std::endl;
   while(1)
     std::this_thread::sleep_for(std::chrono::seconds(5));
