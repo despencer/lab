52f1b22e6fb7445a1e799fbbbe13e9dbccf5c41f
despencer <despencer@mail.ru>
2023-08-08 23:41:31 +0300

    Changed lazy example to external function.

--- python/lazy.py
+++ python/lazy.py
@@ -25,6 +25,10 @@ class Lazy2:
             setattr(instance, self.name, value)
             return value
 
+def fcalc2(instance):
+    print('calc2')
+    return 2
+
 class model:
     def __init__(self):
         pass
@@ -34,12 +38,8 @@ class model:
         print('calc1')
         return 1
 
-    def fcalc2(self):
-        print('calc2')
-        return 2
-
 c = model()
-setattr(model, 'calc2', Lazy2('calc2', model.fcalc2) )
+setattr(model, 'calc2', Lazy2('calc2', fcalc2) )
 print('calc1A =', c.calc1)
 print('calc1B =', c.calc1)
 print('calc2A =', c.calc2)
