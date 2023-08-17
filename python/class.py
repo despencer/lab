f741bb43bcfb6652f3e5fa03221d2014fc5efb22
despencer <despencer@mail.ru>
2023-08-17 19:16:12 +0300

    Base/Derived methods.

--- python/class.py
+++ python/class.py
@@ -7,3 +7,30 @@ class Test:
 
 Test.value = 'a'
 print(Test.value)
+
+class Base:
+    def __init__(self):
+        print('init at Base')
+
+    @classmethod
+    def get(cls):
+        print('get at Base')
+
+    @classmethod
+    def set(cls):
+        print('set at Base')
+        cls()
+
+
+class Derived(Base):
+    def __init__(self):
+        print('init at Derived')
+
+    @classmethod
+    def get(cls):
+        print('get at Derived')
+
+Base.get()
+Derived.get()
+Base.set()
+Derived.set()
