dbf9651f431213b69a58fd4fb732d0be7a72059e
despencer <despencer@mail.ru>
2023-10-14 22:36:14 +0300

    Parametrized generator for c file added.

--- cmake/paragen/generator.py
+++ cmake/paragen/generator.py
@@ -1,16 +1,32 @@
 import jinja2
 import argparse
 import sys
+import yaml
+
+class Options:
+    pass
+
+def getoptions(srcdir, optfile):
+    with open(srcdir + '/settings.yaml') as fset:
+        jset = yaml.load(fset, Loader=yaml.Loader)
+    with open(srcdir + '/' + optfile) as fopt:
+        jopt = yaml.load(fopt, Loader=yaml.Loader)
+    opt = Options()
+    opt.name = jopt['name']
+    opt.greeting = jset['greeting'][jopt['greeting']]
+    return opt
 
 def main():
     parser = argparse.ArgumentParser(description='Generates greetings file')
     parser.add_argument('srcdir', help='source dir')
     parser.add_argument('dstdir', help='destination dir')
+    parser.add_argument('options', help='options yaml')
     args = parser.parse_args()
 
+    options = getoptions(args.srcdir, args.options)
     env = jinja2.Environment(loader=jinja2.FileSystemLoader(args.srcdir))
     with open(args.dstdir + '/main.c', mode='w') as target:
-        target.write(env.get_template('main.c.jinja').render(name="World"))
+        target.write(env.get_template('main.c.jinja').render(options=options))
     return 0
 
 if __name__ == "__main__":
