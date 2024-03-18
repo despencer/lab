d42a396b4b5a929d4cc22c1665a24125e17f42f7
despencer <despencer@mail.ru>
2024-03-18 13:04:27 +0300

    Transfer code to SMContext class procedures.

--- spidermonkey/smjs.cpp
+++ spidermonkey/smjs.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include <jsapi.h>
-#include <js/Initialization.h>
 #include "smjs.h"
+#include <js/SourceText.h>
+#include <js/CompilationAndEvaluation.h>
 
 bool SMContext::init(void)
 {
@@ -17,3 +17,59 @@ void SMContext::shutdown(void)
 {
  JS_ShutDown();
 }
+
+static JSClass SMGlobalClass = { "global", JSCLASS_GLOBAL_FLAGS, &JS::DefaultGlobalClassOps };
+
+SMContext* SMContext::open(void)
+{
+ JSContext* jscontext = JS_NewContext(JS::DefaultHeapMaxBytes);
+ if(jscontext == NULL)
+    {
+    std::cerr << "Can't create JSContext\n";
+    return NULL;
+    }
+
+ SMContext* context = new SMContext();
+ context->context = jscontext;
+
+ JS::InitSelfHostedCode(context->context);
+
+ context->options = new JS::RealmOptions();
+ context->global = JS_NewGlobalObject(context->context, &SMGlobalClass, nullptr, JS::FireOnNewGlobalHook, *context->options);
+ context->root = new JS::RootedObject(context->context, context->global);
+ context->realm = new JSAutoRealm(context->context, *context->root);
+ return context;
+}
+
+void SMContext::close(void)
+{
+ delete this->realm;
+ delete this->root;
+ delete this->options;
+ JS_DestroyContext(this->context);
+ this->context = NULL;
+}
+
+bool SMContext::evaluate(const char* script)
+{
+ JS::CompileOptions* options = new JS::CompileOptions(this->context);
+ options->setFileAndLine("script", 1);
+
+ JS::SourceText<mozilla::Utf8Unit>* source = new JS::SourceText<mozilla::Utf8Unit>();
+ bool ret = source->init(this->context, script, strlen(script), JS::SourceOwnership::Borrowed);
+
+ JS::RootedValue* result =  new JS::RootedValue(this->context);
+
+ if(ret)
+    ret = JS::Evaluate(this->context, *options, *source, result);
+
+ printf("%s\n", JS_EncodeStringToASCII(this->context, result->toString()).get());
+
+ delete result;
+ delete source;
+ delete options;
+
+ return ret;
+}
+
+
--- spidermonkey/smjs.h
+++ spidermonkey/smjs.h
@@ -1,11 +1,24 @@
 #ifndef __SPIDER_MONKEY_JAVASCRIPT__
 #define __SPIDER_MONKEY_JAVASCRIPT__
 
+#include <jsapi.h>
+#include <js/Initialization.h>
+
 class SMContext
 {
+ protected:
+   JSContext* context;
+   JS::RealmOptions* options;
+   JSObject* global;
+   JS::RootedObject* root;
+   JSAutoRealm* realm;
  public:
    static bool init(void);
    static void shutdown(void);
+
+   static SMContext* open(void);
+   void close(void);
+   bool evaluate(const char* script);
 };
 
 #endif
