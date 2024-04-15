03a8104803df880a482bf43a0a0004823585be00
despencer <despencer@mail.ru>
2024-04-15 13:08:23 +0300

    Setup for jQuery test.

---
+++ spidermonkey/python/domexec.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import argparse
+import sys
+import os
+sys.path.insert(1, os.path.expanduser('~/dev/web'))
+sys.path.insert(1, os.path.expanduser('~/dev/web/smjs'))
+import spidermonkey
+import webhtml
+import webdom
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Executes a javascript file inside browser environment')
+    parser.add_argument('html', help='dom file')
+    parser.add_argument('js', help='script file')
+    args = parser.parse_args()
+    with open(args.html) as htmlfile:
+        dom = webhtml.parse(htmlfile.read())
+        with open(args.js) as jsfile:
+            with spidermonkey.connect() as context:
+                try:
+                    webdom.setupcontext(context, dom)
+                    context.execute(jsfile.read())
+                except Exception as e:
+                    print('JS error:', e)
