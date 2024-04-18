a545c2ce55cbe852ff71f56c31db97ec034b8343
despencer <despencer@mail.ru>
2024-04-18 23:41:10 +0300

    Reworked Python binding for JavaScript: make globals available from global Python object.

--- spidermonkey/python/jsexec.py
+++ spidermonkey/python/jsexec.py
@@ -6,32 +6,32 @@ import os
 sys.path.insert(1, os.path.expanduser('~/dev/web/smjs'))
 import spidermonkey
 
-def hello():
-    return 'Hello, world!'
-
 class Document:
     def __init__(self):
-        self._name = 'A name'
+        self.name = 'A name'
         self.base = 'A base'
 
-    @property
-    def name(self):
-        return self._name
+class HelloObject:
+    def __init__(self):
+        self.document = Document()
 
-    @name.setter
-    def name(self, newname):
-        self._name = newname
+    def print(self, str):
+        print(str)
+
+    def hello(self):
+        return 'Hello, world from Python!'
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Reads a javascript file')
     parser.add_argument('js', help='a js file')
     args = parser.parse_args()
     with open(args.js) as jsfile:
+        hello = HelloObject()
         document = Document()
-        with spidermonkey.connect() as context:
+        with spidermonkey.connect(hello) as context:
             try:
-                context.add( {'print':print, 'hello':hello} )
-                context.add( {'document':document} )
+#                context.add( {'print':print, 'hello':hello} )
+#                context.add( {'document':document} )
                 context.execute(jsfile.read())
             except Exception as e:
                 print('JS error:', e)
