b4e8ae3484402344f0b6678512497a983fd814fa
despencer <despencer@mail.ru>
2024-07-02 16:20:26 +0300

    BeautifulSoup example was added.

---
+++ python/bs/bs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import argparse
+from bs4 import BeautifulSoup
+
+def main(soup):
+    print(soup.__class__)
+    print(soup.html.name)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Load an html file')
+    parser.add_argument('html', help='dom file')
+    args = parser.parse_args()
+    with open(args.html) as htmlfile:
+        soup = BeautifulSoup(htmlfile, 'html5lib')
+        main(soup)
