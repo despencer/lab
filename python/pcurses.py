1a216baa557dcbbac9db66ae943e523f196786dd
despencer <despencer@mail.ru>
2025-12-31 18:38:55 +0300

    Added command scroll logic for curses module.

--- python/pcurses.py
+++ python/pcurses.py
@@ -1,27 +1,46 @@
 #!/usr/bin/python3
 import curses
 
-def main(stdscr):
-    cout = curses.newwin(curses.LINES-3, curses.COLS, 0, 0)
-    ccmd = curses.newwin(3, curses.COLS, curses.LINES-3, 0)
-    cout.border()
-    ccmd.border()
-    cout.refresh()
-    ccmd.addstr(1, 1, '>')
-    ccmd.move(1, 2) 
-#    ccmd.border()
-#    stdscr.refresh()
-#    ccmd.refresh()
-    i = 0
-    while True:
-        k = ccmd.getkey()
-        if k == '\n':
-            k = '*'
-        cout.addstr(i+1, 1, k)
-        ccmd.addstr(1, i+2, k)
-        cout.refresh()
-        ccmd.refresh()
-        i += 1
+class CursesInput:
+    def __init__(self):
+        self.history = []
+
+    def open(self):
+        self.cout = curses.newwin(curses.LINES-3, curses.COLS, 0, 0)
+        self.ccmd = curses.newwin(3, curses.COLS, curses.LINES-3, 0)
+        self.cout.border()
+        self.ccmd.border()
+        self.cout.refresh()
+        self.ccmd.addstr(1, 1, '>')
+        self.ccmd.move(1, 2)
+
+    def loop(self, stdscr):
+        self.open()
+        while True:
+            line = ''
+            while True:
+                key = self.ccmd.getkey()
+                if key == '\n':
+                    break
+                self.ccmd.addstr(1, 2+len(line), key)
+                line += key
+                self.ccmd.refresh()
+            if line == 'quit':
+                break
+            self.addline(line)
+            self.ccmd.addstr(1, 1, '>' + ' ' * (curses.COLS-3))
+            self.ccmd.move(1, 2)
+
+    def shell(self):
+        curses.wrapper(self.loop)
+
+    def addline(self, line):
+        self.history.append(line)
+        wnd = curses.LINES-5
+        for i in range(wnd):
+            if i < len(self.history):
+                self.cout.addstr(wnd-i, 1, self.history[ -i-1 ].ljust(curses.COLS-2) )
+        self.cout.refresh()
 
 if __name__ == '__main__':
-    curses.wrapper(main)
+    CursesInput().shell()
