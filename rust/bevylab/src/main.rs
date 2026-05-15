3358e571bf49b98c81aa33edf37a8138659b1e87
despencer <despencer@mail.ru>
2026-05-15 23:36:42 +0300

    Bevy usage added.

--- rust/bevylab/src/main.rs
+++ rust/bevylab/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use bevy::prelude::*;
+
+fn hello_bevy()
+{
+ println!("Hello, Bevy!");
+}
+
+fn main()
+{
+ App::new().add_system(hello_bevy).run();
 }
